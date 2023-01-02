a6ad9c7ecbf30adb6d71914dd52920b58a5e53dd
SandraBotica <smbotica@gmail.com>
2023-01-02 18:06:54 +0800

    2Jan

---
+++ PyBank/main.py
@@ -0,0 +1,140 @@
+import os
+import csv
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+    
+    # net_total_amount = int(budget_data[1])
+    # date =str(budget_data[0])
+    # monthly_change = int
+    # average_monthly_change = int({monthly_change}/{total_number_months})
+    # increase_profits = int     
+    # greatest_increase_profits = int
+    # decrease_profits = int     
+    # greatest_decrease_profits = int
+    
+# with open(csvpath) as csvfile:
+#     csvreader = csv.reader (csvfile)
+#     print(csvreader)
+#     csv_header = next(csvreader)
+    
+#     print (f"CSV Header: {csv_header}")
+    
+#     # for item in csvreader:
+#     #     print(item [0], item[1])
+    
+#     total_number_months = 0
+    
+#     for row in csvreader:
+#         total_number_months = total_number_months + 1
+#     print (f"Total months: ", int(total_number_months))
+    
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.DictReader (csvfile, delimiter=',')
+    
+    total_number_months = 0
+    monthly_amount = []
+    net_total_amount = 0
+    monthly_change = []
+    
+    for row in csvreader:
+        total_number_months = total_number_months + 1
+        monthly_amount.append (row['Profit/Losses'])
+        # monthly_change.append (row['Profit/Losses'] + next[monthly_amount])
+    
+    print (f"Total months: ", int(total_number_months))
+    print(monthly_amount)
+    # print(monthly_change)
+    
+    for numbers in monthly_amount:
+        sum = sum + numbers
+            # net_total_amount = net_total_amount + monthly_amount
+
+    print(sum)
+    # print(net_total_amount)
+    
+    # net_total_amount = sum([monthly_amount])
+    # print(net_total_amount)
+    
+    
+ 
+        
+
+    
+    # budgetData = list(csvreader)
+    
+    # net_total_amount = 0
+    
+    # for row in csvreader:
+    #     net_total_amount = net_total_amount + next(budgetData[0][1])
+        
+
+    
+    # net_total_amount = 0
+    
+    # for row in csvreader:
+    #     net_total_amount = net_total_amount + int(row[1])
+          
+    # count = 0
+    # for row in csvreader:
+        # if count == 0:
+            # else:
+                # count += 1
+                # print(f'{count}')
+    
+    # net_total_amount = 0
+    # amount = (item[1])
+    # for amount in csv reader:
+        # append.amount
+        # net_total_amount = sum(item[1])
+    
+    # monthly_change = 
+    # should we be writing this into the csv i.e. row[1] add to next row
+    # save this value 
+    # average_monthly_change = int({monthly_change}/{total_number_months})
+    
+    # date =
+    # increase_profits = 0  how to obtain first positive value in column monthly change
+    # greatest_increase_in_profits ={}
+    # greatest_increase_in_profits ={"Date": "amount"}
+    # for row in csvreader:
+        # if (monthly_change) is >0 and > increase_profits:
+            # append.increase_profits with monthly_change and append.date
+            # greatest_increase_profits = (increase_profits)
+        # else:
+        
+    # date =
+    # decrease_profits = 0  how to obtain first negative value in column monthly change
+    # greatest_decrease_in_profits ={}
+    # greatest_decrease_in_profits ={"Date": "amount"}
+    # for row in csvreader:
+        # if (monthly_change) is <0 and > decrease_profits:
+            # append.decrease_profits with monthly_change and append.date
+            # greatest_decrease_profits = (decrease_profits)
+        # else:
+    
+    # net_total_amount = []
+    # profits = []
+    # losses = []
+    # for row in csvreader:  
+    #     for item in csvreader:
+    #         amount =int(item[1])
+    #         if amount > 0:
+    #             profits.append
+    #         else:
+    #             losses.append
+    # print(profits) 
+    # print(losses)
+      
+    # print("Financial Analysis")
+        
+    # print("_____________________________________")
+      
+    # print (f"Total months: ", int(total_number_months))
+    
+    # print(f"Total: $ ", int(net_total_amount))
+    
+    # print (f"Average change: $ int{average_monthly_change}")
+    
+    # print (f"Greatest Increase in profits : {date} ($ int{greatest_increase-profits})")
+    
+    # print (f"Greatest Decrease in profits : {date} ($ int{greatest_decrease-profits})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,152 @@
-print "hello"
+import os
+import csv
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+  # candidate_name = str(election_data[2])
+	# candidate_votes = int()
+	# candidate_percent_votes = int()
+	# winning_candidate = str()
+ 
+with open(csvpath) as csvfile:
+  csvreader = csv.reader(csvfile)
+  print(csvreader)
+  csv_header = next(csvreader)
+    
+  print (f"CSV Header: {csv_header}")
+  print("_____________________________________")
+
+  # dict={f"Candidate":str[" "], "candidate_votes":int[0], "candidate_percent_votes":float[0]}
+  # candidates = {f'str[candidate_name]:int[candidate_votes]: int[candidate_percent_votes]'}
+  
+  total_votes = 0
+  name_count = {}
+  # candidate_name = []
+  # candidate_votes = 0
+  # candidate_percent_votes = [] 
+  
+  for row in csvreader:
+    total_votes = total_votes + 1
+    candidate_name = row[2]
+    for name in candidate_name:
+      if name in name_count:
+        name_count[name] += 1
+      else:
+        name_count[name] = 1
+        
+        
+      # if candidate_name == candidate_name:
+      # name_count = candidate_name
+      
+  print (f"Total votes: ",int(total_votes))
+  print (name_count)
+  
+  # for item in csvreader:
+  #     print (item[2])
+    
+  # for row[2] in csvreader:
+  #   if 'Candidate' == 'Candidate':
+  #     candidate_votes = candidate_votes + 1
+  # print(f"['candidate_name'], int[candidate_votes]")
+  
+    #   candidate_name.append
+    #   if 'Candidate' == Candidate:
+    #     candidate_votes.append('Candidate")
+    #   else: 
+    #     candidate_name.append
+    # for candidate_name, candidate_votes, candidate_percent_votes in candidates.items():
+    #   print(f"{candidate_name} ": " {candidate_percent_votes} " %" {candidate_votes}")
+    
+    
+
+# with open(csvpath, newline='') as csvfile:
+#   csvreader = csv.DictReader (csvfile, delimiter=',')
+  
+#   column_names=["{csv_header}"]
+#   dict ={}
+#   poll = {'Ballot ID':[],'County':[], 'Candidate':[]}
+   
+#   for Candidate in poll:
+#     candidate_name = poll[Candidate]
+#     print (candidate_name)
+  # candidate_name = ['Candidate'] 
+  
+  # for row in csvreader:
+  #   if (row['Candidate']) != (row['Candidate']):
+  #     candidate_name.append
+        
+        
+# column_names=["{csv_header}"]
+# dict = {'Ballot ID':[],'County':[], 'Candidate':[]}
+
+# with open(csvpath) as csvfile:
+#     dict_object = csv.DictWriter(csvfile, columnnames = column_names)
+#     dict_object.writerow(dict)
+    
+    
+    
+    # for item in csvreader:
+    #   print(item[2])
+        
+   
+      # if (item[2]) == (item[2]):
+      #   count = count +1
+      #   print(count)
+      
+    # dict={f"Candidate":str[], "candidate_votes":int[], "candidate_percent_votes":float[]}
+    # candidates = {f'str[candidate_name]:int[candidate_votes]: int[candidate_percent_votes]'}
+    # candidate_name = []
+    # candidate_votes = 0
+    # candidate_percent_votes = []
+    
+    # 
+    # for row in csvfile:
+    #   if 'Candidate' == 'Candidate':
+    #     candidate_votes = candidate_votes + 1
+    #     print(f"candidate_votes")
+      
+    #   candidate_name.append
+    #   if 'Candidate' == Candidate:
+    #     candidate_votes.append('Candidate")
+    #   else: 
+    #     candidate_name.append
+    # for candidate_name, candidate_votes, candidate_percent_votes in candidates.items():
+    #   print(f"{candidate_name} ": " {candidate_percent_votes} " %" {candidate_votes}")
+    
+    
+    #     candidate_votes_count = {}
+    #     total_vote_counts =()
+        
+    # for row in csvfile:
+    #     candidate_name = row[2]
+    #     if candidate_name not in candidate_votes_count:
+    #         candidate_votes_count[candidate_name]=1
+    #     else:
+    #         candidate_votes_count[candidate_name]+=1
+    
+    # total_votes=(len(candidate_votes_count{}))  
+    # candidate_percent_votes = (f"{total_votes}/{candidate_votes}%"))
+        
+    #find the candidate_name with the highest candidate_percent_votes
+    # winning_candidate = 
+        
+        
+        
+    
+    # print("Election Results")
+        
+    # print("_____________________________________")
+    
+    # print (f"Total votes: ",int(total_votes))
+        
+    # print("_____________________________________")
+    
+    # print(f"{candidate_name} : {candidate_percent_votes} {candidate_votes}")
+        
+    # print("_____________________________________")
+        
+    # print(f"Winner : {winning_candidate}")
+    
+   
+     
+      
+        
